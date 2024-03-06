09d180367ea2da0fca06e6e9d0de564f972e6fc3
José Salvador Mariscal Pérez <89020962+Salvador025@users.noreply.github.com>
2024-03-06 12:19:18 -0600

    Chava (#11)
    * feat: First test case for check_number_status
    
    * feature: First test case for check_number_status

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
+import functions
 
+# Test for check_number_status
+assert functions.check_number_status(1) == "Positive"
+assert functions.check_number_status(0) == "Zero"
+assert functions.check_number_status(-1) == "Negative"
